9ef5eaa87d34ad613d0a61f1a34646fd2dfa06b4
m.zhitenev <m.zhitenev23@gmail.com>
2023-10-19 16:46:19 +0300

    Залить мастер

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, Мир!");
+        MobilePhone mobilePhone1 = new MobilePhone(5,"чёрный",2010);
+        MobilePhone mobilePhone2 = new MobilePhone(5,"чёрный",2010);
+        LandLinePhone landLinePhone = new LandLinePhone(15);
+
+        int a = 5;
+        int b = 5;
+        System.out.println(a==b);
+        System.out.println(mobilePhone1==mobilePhone2);
+        System.out.println(mobilePhone1);
+        System.out.println(mobilePhone2);
+        System.out.println(mobilePhone1.equals(mobilePhone2));
+
+        mobilePhone1.call(0);
     }
 }
+
